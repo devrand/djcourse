0660fa7c4b2519f9bcd1eb8d5d1ec7b4797c26cf
Anatoly Bondarenko <devrand@gmail.com>
2016-03-24 15:21:29 +0200

    Update sms.py

--- code/sms/sms.py
+++ code/sms/sms.py
@@ -8,12 +8,13 @@ from course_config import *
 
  
 
-# need TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, twilio from_ telephone number
+# u need TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, twilio from_ telephone number
+# u could obtain it on twilio site
  
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN) 
  
 client.messages.create(
-	to="+380501112222", 
-	from_="+1212121212", 
-	body="python sender",  
+	to="+380501112222", # change it
+	from_="+1212121212", # change it 
+	body="python sender",  # this is a body of your sms message
 )
